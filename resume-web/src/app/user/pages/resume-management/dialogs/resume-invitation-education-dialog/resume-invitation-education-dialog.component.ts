b52d521283bde742679cbeb65b135e1dc48efeef
Reymond <reymond.kao@gmail.com>
2022-10-06 01:25:44 +0800

    Feat 處理user履歷管理切版

--- resume-web/src/app/user/pages/resume-management/dialogs/resume-invitation-education-dialog/resume-invitation-education-dialog.component.ts
+++ resume-web/src/app/user/pages/resume-management/dialogs/resume-invitation-education-dialog/resume-invitation-education-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ISelectOption } from '@app/core/interfaces/select-option';
 
 @Component({
   selector: 'app-resume-invitation-education-dialog',
@@ -9,6 +10,12 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class ResumeInvitationEducationDialogComponent implements OnInit {
 
   isSuccess: boolean = false;
+
+  educationOptions: ISelectOption[] = [
+    { text: '國小', key: 0 },
+    { text: '國中', key: 1 },
+    { text: '高中', key: 2 },
+  ];
 
   constructor(
     public dialogRef: MatDialogRef<ResumeInvitationEducationDialogComponent>,
